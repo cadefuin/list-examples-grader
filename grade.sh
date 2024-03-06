fe228ba61261e37dabadde0109e2391570594312
cadefuin <156235788+cadefuin@users.noreply.github.com>
2024-03-06 11:11:25 -0800

    Polished grading script

--- grade.sh
+++ grade.sh
@@ -1,16 +1,45 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
+# Clone student repository to student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# Check right file submitted
+if ! [ -f student-submission/ListExamples.java ]
+then
+    echo "Missing ListExamples.java in student submission."
+    echo "Score: 0"
+    exit
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+# Copy files into grading-area
+cp student-submission/*.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+
+# Compile tests and student code
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]
+then
+    echo "Compilation Error"
+    echo "Score: 0"
+    exit
+fi
+
+# Run tests and report grade
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > error-output.txt
+head -n 2 error-output.txt | tail -n 1 > test-results.txt
+MAX=$(tr -c -d '.' < test-results.txt | wc -c)
+ERRORS=$(tr -c -d 'E' < test-results.txt | wc -c)
+if [ $ERRORS -ne 0 ]
+then
+    cat error-output.txt
+fi
+echo "Score:" $(( $MAX - $ERRORS )) "/" $MAX
